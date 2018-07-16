e2311949e6a00292a79f7c6cccb2cebf7a886f07
Kyulix <none@none.com>
2018-07-16 21:32:28 +0300

    Blank application added

---
+++ src/main/java/com/kyulix/RGTestApp/RgTestAppApplication.java
@@ -0,0 +1,12 @@
+package com.kyulix.RGTestApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RgTestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RgTestAppApplication.class, args);
+	}
+}
